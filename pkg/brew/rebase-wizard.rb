a9ff3a72aac9a19dbc0c75b75224b77644af2b51
Alex Burkhart <saterus@gmail.com>
2020-04-13 00:53:20 -0400

    Bump homebrew release to 1.0.0

--- pkg/brew/rebase-wizard.rb
+++ pkg/brew/rebase-wizard.rb
@@ -1,14 +1,14 @@
 class RebaseWizard < Formula
   desc "Let the Wizard guide you through the darkest of gituations"
   homepage "https://github.com/saterus/rebase-wizard"
-  url "https://github.com/saterus/rebase-wizard/archive/v0.99.2.tar.gz"
-  sha256 "aa6b4fd58b30c7b0e2e9a7211075b8245f735ba9193bebafc011035a730dbf5b"
+  url "https://github.com/saterus/rebase-wizard/archive/v1.0.0.tar.gz"
+  sha256 "bb57a2080fa37e0bf2eb1250ba60690e023f1581a5426ee95da05a7d07743f4a"
   head "git@github.com:saterus/rebase-wizard.git"
 
   bottle do
-    root_url "https://github.com/saterus/rebase-wizard/releases/download/v0.99.2/"
+    root_url "https://github.com/saterus/rebase-wizard/releases/download/v1.0.0/"
     cellar :any_skip_relocation
-    sha256 "5fac691cdc0cb042ea83612ac859f294f1958931644db1d9ffcb1767e912452e" => :mojave
+    sha256 "d11c2033dd2887ac25185f8fcf1334b16e1b1e7379876b0bdcca64703f481559" => :mojave
   end
 
   depends_on "rust" => [:build]
